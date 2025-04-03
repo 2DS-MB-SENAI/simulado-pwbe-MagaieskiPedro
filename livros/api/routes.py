2de7fa97c43f019458cc19e1afd44bfae38c4f23
Pedro Alves Estevam Magaieski <46140459877@>
2025-04-03 09:05:41 -0300

    removendo diretorios

--- livros/api/routes.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('auth/registro/',view=views.registrar, name="registro"),
-    path('auth/login/',view=views.login, name="login"),
-]
